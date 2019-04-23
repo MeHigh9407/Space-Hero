59c87c5c40784cfb906be16b1042c43ecaa887bb
MeHigh94 <bucur.mihai.gabriel94@gmail.com>
2019-04-23 20:58:47 +0300

    Half of the jump animation works( jumps only from idle) needs more corrections.

--- Assets/Stylized Astronaut/Character/Player.cs
+++ Assets/Stylized Astronaut/Character/Player.cs
@@ -3,40 +3,40 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
-	private Animator anim;
-	private CharacterController controller;
+		private Animator anim;
+		private CharacterController controller;
 
-	public float speed = 600.0f;
-	public float turnSpeed = 600.0f;
-	private Vector3 moveDirection = Vector3.zero;
-	public float gravity = 20.0f;
+		public float speed = 600.0f;
+		public float turnSpeed = 400.0f;
+		private Vector3 moveDirection = Vector3.zero;
+		public float gravity = 20.0f;
 
-		void Start ()
-        {
-        
-		controller = GetComponent <CharacterController>();
-		anim = gameObject.GetComponentInChildren<Animator>();
+		void Start () {
+			controller = GetComponent <CharacterController>();
+			anim = gameObject.GetComponentInChildren<Animator>();
 		}
 
 		void Update (){
-			if (Input.GetKey ("w")) {
-				anim.SetInteger ("AnimationPar", 1);
-			}  else if(Input.GetKeyDown("space"))
-            {
-             anim.SetInteger("AnimationPar", 2);
-            
-            } 
-            
-        else{ 
-		    anim.SetInteger ("AnimationPar", 0);
+        if (Input.GetKey("w"))
+        {
+            anim.SetInteger("AnimationPar", 1);
         }
-         
+        else if (Input.GetKeyDown("space"))
+        {
+            anim.SetBool("PlayerJump", true);
+        }
 
-        if (controller.isGrounded){
+        else
+        {
+            anim.SetInteger("AnimationPar", 0);
+            anim.SetBool("PlayerJump", false);
+        }
+
+			if(controller.isGrounded){
 				moveDirection = transform.forward * Input.GetAxis("Vertical") * speed;
-			}       
+			}
 
-        float turn = Input.GetAxis("Horizontal");
+			float turn = Input.GetAxis("Horizontal");
 			transform.Rotate(0, turn * turnSpeed * Time.deltaTime, 0);
 			controller.Move(moveDirection * Time.deltaTime);
 			moveDirection.y -= gravity * Time.deltaTime;
